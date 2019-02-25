f320285392bfda7f1e9a3cf14182f025ba48fab9
pcrist13 <pcrist@umich.edu>
2019-02-24 18:58:31 -0700

    Parsing and receiving data

--- backend/application.py
+++ backend/application.py
@@ -1,8 +1,11 @@
 # Start with a basic flask app webpage.
-from flask import Flask, render_template, url_for, copy_current_request_context, request
+from flask import Flask, render_template, url_for, copy_current_request_context, request, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
 import os
+import tarfile
+from email.parser import Parser
+import json
 
 UPLOAD_FOLDER = '/uploads'
 ALLOWED_EXTENSIONS = set(['gz'])
@@ -19,10 +22,46 @@ def allowed_file(filename):
 @app.route('/api/upload', methods=['GET', 'POST'])
 @cross_origin(headers=['Content-Type'])
 def fileUpload():
-    if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    try:
+        if request.method == 'POST':  
+            importFile = request.files['file']
+            if importFile:
+                filename = secure_filename(importFile.filename)
+                importFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                
+                if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+                    print "File exists"
+
+                tar = tarfile.open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "r:gz")
+                resp = []
+                for member in tar.getmembers():
+                    f = tar.extractfile(member)
+                    if f is not None:
+                        content = f.read()
+                        
+                        message = Parser().parsestr(content)
+                        messageId = message['message-id']
+                        to = message['to']
+                        sender = message['from']
+                        subject = message['subject']
+                        date = message['date']
+
+                        message_dictionary = {
+                            "msg-id": messageId,
+                            "to": to,
+                            "sender": sender,
+                            "subject": subject,
+                            "date": date
+                        }
+                        
+                        resp.append(message_dictionary)
+                        resp = json.dumps(resp)
+                        resp = json.loads(resp)
+
+                return jsonify(resp)
+
+    except Exception, e:
+        return str(e)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
