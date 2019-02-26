036ac59d1cd40b03cedd86cb184dd16b81330551
pcrist13 <pcrist@umich.edu>
2019-02-26 12:31:31 -0700

    Removed extraneous cors decorator and exception handling that gobbled errors

--- backend/application.py
+++ backend/application.py
@@ -9,7 +9,6 @@ UPLOAD_FOLDER = '/uploads'
 ALLOWED_EXTENSIONS = set(['gz', 'msg'])
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app, resources={r'/api/upload': {'origins': 'http://localhost:8080'}})
@@ -18,21 +17,15 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/api/upload', methods=['GET', 'POST'])
-@cross_origin(headers=['Content-Type'])
 def fileUpload():
-    try:
-        if request.method == 'POST':  
-            importFile = request.files['file']         
-            if importFile.content_type == "application/x-gzip" or importFile.content_type == "application/octet-stream":
-                parser = parser_factory.FileParser.factory(importFile.content_type)
-                resp = parser.parse(importFile, UPLOAD_FOLDER)
-                resp = json.dumps(resp)
-                resp = json.loads(resp)
-                return jsonify(resp)
-
-    except Exception, e:
-        return str(e)
-
+    if request.method == 'POST':  
+        importFile = request.files['file']         
+        if importFile.content_type == "application/x-gzip" or importFile.content_type == "application/octet-stream":
+            parser = parser_factory.FileParser.factory(importFile.content_type)
+            resp = parser.parse(importFile, UPLOAD_FOLDER)
+            resp = json.dumps(resp)
+            resp = json.loads(resp)
+            return jsonify(resp)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
